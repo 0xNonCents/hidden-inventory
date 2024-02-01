ee096c0f61bd4c677e290cc6a004d7f16e19d06c
0xNonCents <0xNonCents@users.noreply.github.com>
2024-02-01 19:34:20 +0000

    deploy: 01968e5cbae8702be84df19f477e36c755f8cf06

--- src/contracts/AddNumbers.s.sol
+++
@@ -1,9 +0,0 @@
-// SPDX-License-Identifier: MIT
-pragma solidity >0.8.0;
-
-contract AddNumbers {
-    // Function to add two numbers
-    function add(uint256 a, uint256 b) public pure returns (uint256) {
-        return a + b;
-    }
-}
